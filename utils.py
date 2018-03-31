b934318187d7727c8015d2ae5428777521d8f323
Will LeVine <levinwil2@gmail.com>
2018-03-31 12:28:37 -0500

    finished k means

--- utils.py
+++ utils.py
@@ -43,8 +43,8 @@ def plot_decision_boundary(X, y, clf):
     # point in the mesh [x_min, x_max]x[y_min, y_max].
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, .02),
-                         np.arange(y_min, y_max, .02))
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, .01),
+                         np.arange(y_min, y_max, .01))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
     # Put the result into a color plot
